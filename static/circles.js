1ddd549705d7bf6a5f4a040f989e69937644e9fb
minima <benjamean.dev@gmail.com>
2026-08-09 17:26:13 +0100

    readjusted overscroll behaviors

--- static/circles.js
+++ static/circles.js
@@ -24,7 +24,7 @@ function createPageBackground() {
       radius: 15 + Math.random() * 35,
       vx: (Math.random() - 0.5) * 4,
       vy: (Math.random() - 0.5) * 4,
-      opacity: 0.15 * (0.5 + Math.random() * 0.5)
+      opacity: 0.06 * (0.5 + Math.random() * 0.5)
     };
   }
 
